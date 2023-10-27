d21eaa96ca0601cd4eff7bef61ad4d72011e59c4
Bhanuprakashreddyv <v.bhanureddy184@gmail.com>
2023-10-27 22:26:43 +0100

    Add Skills and Grid to 6 per column.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,6 +5,13 @@ import css from '../assets/css.png';
 import javascript from '../assets/javascript.png';
 import reactImage from '../assets/react.png';
 import github from '../assets/github.png';
+import Sql from '../assets/Sql.png';
+import tailwind from '../assets/tailwind.png';
+import Git from '../assets/Git.png';
+import Bootstrap from '../assets/Bootstrap.jpeg';
+import Trello from '../assets/Trello.jpeg';
+import Figma from '../assets/Figma.png';
+
 
 const Skills = () => {
     const techs = [
@@ -38,6 +45,42 @@ const Skills = () => {
             title: 'GitHub',
             style: 'shadow-gray-400'
         },
+        {
+            id: 6,
+            src: Sql,
+            title: 'SQL',
+            style: 'shadow-blue-300'
+        },
+        {
+            id: 7,
+            src: tailwind,
+            title: 'Tailwind',
+            style: 'shadow-green-300'
+        },
+        {
+            id: 8,
+            src: Git,
+            title: 'Git',
+            style: 'shadow-red-300'
+        },
+        {
+            id: 9,
+            src: Bootstrap,
+            title: 'Bootstrap',
+            style: 'shadow-violet-300'
+        },
+        {
+            id: 10,
+            src: Trello,
+            title: 'Trello',
+            style: 'shadow-blue-400'
+        },
+        {
+            id: 11,
+            src: Figma,
+            title: 'Figma',
+            style: 'shadow-orange-400'
+        },         
     ]
   return (
     <div name="skills" 
@@ -47,7 +90,7 @@ const Skills = () => {
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Skills</p>
                 <p className='py-6'>These are the Skills i'm familiar with!</p>
             </div>
-            <div className='grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-4 sm:px-0'>
+            <div className='grid grid-cols-2 sm:grid-cols-6 gap-8 text-center py-8 px-4 sm:px-0'>
                 {
                     techs.map(({id, src, title, style}) => (
                 <div key={id} 
