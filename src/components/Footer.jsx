f0f109c32bbb68cb7340db415418becc97d63681
Bhanuprakashreddyv <v.bhanureddy184@gmail.com>
2023-11-18 07:47:07 +0000

    add styles for footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = () => {
   return (
     <div className="bg-gray-800 text-white py-4">
       <div className="max-w-screen-lg mx-auto flex flex-col sm:flex-row justify-between items-center px-4">
-        <div className="flex items-center space-x-4 mb-4 sm:mb-0">
+        {/* Display only for smaller screens */}
+        <div className="flex items-center space-x-4 mb-4 sm:hidden">
           {/* GitHub */}
           <a
             href="https://github.com/Bhanuprakashreddyv"
@@ -30,8 +31,8 @@ const Footer = () => {
           <a href="mailto:v.bhanureddy184@gmail.com" className="hover:text-gray-500">
             <HiOutlineMail size={30} />
           </a>
-            {/* Resume */}
-            <a
+          {/* Resume */}
+          <a
             href="/Bhanu_VENNAPUSA Resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
@@ -40,6 +41,42 @@ const Footer = () => {
             <BsFillPersonLinesFill size={30} />
           </a>
         </div>
+
+        {/* Hide on smaller screens and display on larger screens */}
+        <div className="hidden sm:flex items-center space-x-4">
+          {/* GitHub */}
+          <a
+            href="https://github.com/Bhanuprakashreddyv"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-500"
+          >
+            <FaGithub size={30} />
+          </a>
+          {/* LinkedIn */}
+          <a
+            href="https://linkedin.com/in/bhanu-prakash-reddy-vennapusa"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-500"
+          >
+            <FaLinkedin size={30} />
+          </a>
+          {/* Email */}
+          <a href="mailto:v.bhanureddy184@gmail.com" className="hover:text-gray-500">
+            <HiOutlineMail size={30} />
+          </a>
+          {/* Resume */}
+          <a
+            href="/Bhanu_VENNAPUSA Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-500"
+          >
+            <BsFillPersonLinesFill size={30} />
+          </a>
+        </div>
+
         <div>
           <p className="text-sm">
             &copy; {new Date().getFullYear()} Bhanu Prakash Reddy VENNAPUSA. All rights reserved.
